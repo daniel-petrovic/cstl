94e4662fabf6888074bf30381ec10cbb93ceee19
Daniel Petrovic <daniel-dev@hotmail.de>
2022-11-29 21:16:55 +0000

    (cstl_vector) Added doxygen comments

--- include/cstl/vector.h
+++ include/cstl/vector.h
@@ -6,23 +6,112 @@
 struct cstl_vector;
 
 // Creation/Deletion
+/**
+ * @brief Allocate a new vector. Caller is reponsible for freeing the vector.
+ * @see cstl_vector_delete
+ * 
+ * @param elem_size 
+ * @return struct cstl_vector* 
+ */
 struct cstl_vector *cstl_vector_new(size_t elem_size);
-void cstl_vector_clear(struct cstl_vector *);
-void cstl_vector_delete(struct cstl_vector *);
 
+/**
+ * @brief Delete the vector and it's associated memory.
+ * 
+ * @param vec 
+ */
+void cstl_vector_delete(struct cstl_vector *vec);
+
+/**
+ * @brief Get the size in bytes of the contained element type.
+ * 
+ * @param vec 
+ * @return size_t 
+ */
 size_t cstl_vector_elem_size(const struct cstl_vector *vec);
+
+/**
+ * @brief Get the number (not bytes!) of elements in the vector.
+ *        The number of bytes can be calculated with cont * esize
+ * @param vec 
+ * @return size_t 
+ */
 size_t cstl_vector_elem_count(const struct cstl_vector *vec);
+
+/**
+ * @brief Get the max number of elements (not bytes!) which
+ *        could be inserted in the vector without reallocation.
+ * 
+ * @param vec 
+ * @return size_t 
+ */
 size_t cstl_vector_elem_capacity(const struct cstl_vector *vec);
+
+/**
+ * @brief  Check if vector is empty.
+ * 
+ * @param vec 
+ * @return int - non-zero if empty, otherwise 0
+ */
 int cstl_vector_empty(const struct cstl_vector *vec);
 
 // Modifier
+/**
+ * @brief Set the vector capacity in number of elements (not bytes)!. 
+ *        Does nothing if count < current capacity.
+ * 
+ * @param vec 
+ * @param count 
+ */
 void cstl_vector_reserve(struct cstl_vector *vec, size_t count);
+
+/**
+ * @brief Push the element at the end.
+ * 
+ * @param vec 
+ * @param elem 
+ */
 void cstl_vector_push(struct cstl_vector *vec, const void *elem);
+
+/**
+ * @brief Pop and return a pointer to the last element.
+ *        The pointer is invalidated if an reallocation happens after the pop operation,
+ *        for ex. due a new call to reserve.
+ * 
+ * @param vec 
+ * @return void* 
+ */
 void *cstl_vector_pop(struct cstl_vector *vec);
 
 // Element access
+/**
+ * @brief Get a pointer to the last element.
+ *        The pointer is invalidated if an reallocation happens after the pop operation,
+ *        for ex. due a new call to reserve.
+ * 
+ * @param vec 
+ * @return void* 
+ */
 void *cstl_vector_back(struct cstl_vector *vec);
+
+/**
+ * @brief Get a pointer to the first element.
+ *        The pointer is invalidated if an reallocation happens after the pop operation,
+ *        for ex. due a new call to reserve.
+ * 
+ * @param vec 
+ * @return void* 
+ */
 void *cstl_vector_front(struct cstl_vector *vec);
+
+/**
+ * @brief Get a pointer to the element at a specified index.
+ *        The pointer is invalidated if an reallocation happens after the pop operation,
+ *        for ex. due a new call to reserve.
+ * 
+ * @param index 
+ * @return void* 
+ */
 void *cstl_vector_at_or_null(struct cstl_vector *, size_t index);
 
 #endif
--- src/vector.c
+++ src/vector.c
@@ -8,113 +8,94 @@
 
 struct cstl_vector
 {
-  size_t esize;     // size of single element in bytes
-  size_t ecount;    // number of elements
-  size_t ecapacity; // max number of elements
-  void *data;
+    size_t esize;     // size of single element in bytes
+    size_t ecount;    // number of elements
+    size_t ecapacity; // max number of elements
+    void *data;
 };
 
-struct cstl_vector *
-cstl_vector_new (size_t elem_size)
+struct cstl_vector *cstl_vector_new(size_t elem_size)
 {
-  struct cstl_vector *vec = calloc (1, sizeof (struct cstl_vector));
-  vec->esize = elem_size;
-  vec->ecount = 0;
-  vec->ecapacity = 0;
-  vec->data = NULL;
-  return vec;
+    struct cstl_vector *vec = calloc(1, sizeof(struct cstl_vector));
+    vec->esize = elem_size;
+    vec->ecount = 0;
+    vec->ecapacity = 0;
+    vec->data = NULL;
+    return vec;
 }
 
-void
-cstl_vector_clear (struct cstl_vector *vec)
+void cstl_vector_delete(struct cstl_vector *vec)
 {
-  free (vec->data);
-  memset (vec, 0, sizeof (vec));
+    free(vec->data);
+    free(vec);
 }
 
-void
-cstl_vector_delete (struct cstl_vector *vec)
+size_t cstl_vector_elem_size(const struct cstl_vector *vec)
 {
-  cstl_vector_clear (vec);
-  free (vec);
+    return vec->esize;
 }
 
-size_t
-cstl_vector_elem_size (const struct cstl_vector *vec)
+size_t cstl_vector_elem_count(const struct cstl_vector *vec)
 {
-  return vec->esize;
+    return vec->ecount;
 }
 
-size_t
-cstl_vector_elem_count (const struct cstl_vector *vec)
+size_t cstl_vector_elem_capacity(const struct cstl_vector *vec)
 {
-  return vec->ecount;
+    return vec->ecapacity;
 }
 
-size_t
-cstl_vector_elem_capacity (const struct cstl_vector *vec)
+int cstl_vector_empty(const struct cstl_vector *vec)
 {
-  return vec->ecapacity;
+    return vec->ecount == 0;
 }
 
-int
-cstl_vector_empty (const struct cstl_vector *vec)
+void cstl_vector_reserve(struct cstl_vector *vec, size_t count)
 {
-  return vec->ecount == 0;
-}
-
-void
-cstl_vector_reserve (struct cstl_vector *vec, size_t count)
-{
-  if (count > vec->ecapacity)
+    if (count > vec->ecapacity)
     {
-      vec->data = realloc (vec->data, count);
-      vec->ecapacity = count;
+        vec->data = realloc(vec->data, count);
+        vec->ecapacity = count;
     }
 }
 
-void
-cstl_vector_push (struct cstl_vector *vec, const void *elem)
+void cstl_vector_push(struct cstl_vector *vec, const void *elem)
 {
-  if (vec->ecount >= vec->ecapacity)
+    if (vec->ecount >= vec->ecapacity)
     {
-      cstl_vector_reserve (vec, MAX (1, 2 * vec->ecapacity));
+        cstl_vector_reserve(vec, MAX(1, 2 * vec->ecapacity));
     }
 
-  void *back = vec->data + vec->esize * vec->ecount;
-  memcpy (back, elem, vec->esize);
-  ++vec->ecount;
+    void *back = vec->data + vec->esize * vec->ecount;
+    memcpy(back, elem, vec->esize);
+    ++vec->ecount;
 }
 
-void *
-cstl_vector_pop (struct cstl_vector *vec)
+void *cstl_vector_pop(struct cstl_vector *vec)
 {
-  assert (vec->ecount > 0);
-  void *back = cstl_vector_back (vec);
-  --vec->ecount;
-  return back;
+    assert(vec->ecount > 0);
+    void *back = cstl_vector_back(vec);
+    --vec->ecount;
+    return back;
 }
 
-void *
-cstl_vector_back (struct cstl_vector *vec)
+void *cstl_vector_back(struct cstl_vector *vec)
 {
-  assert (vec->ecount > 0);
-  return vec->data + (vec->ecount - 1) * vec->esize;
+    assert(vec->ecount > 0);
+    return vec->data + (vec->ecount - 1) * vec->esize;
 }
 
-void *
-cstl_vector_front (struct cstl_vector *vec)
+void *cstl_vector_front(struct cstl_vector *vec)
 {
-  assert (vec->ecount > 0);
-  return vec->data;
+    assert(vec->ecount > 0);
+    return vec->data;
 }
 
-void *
-cstl_vector_at_or_null (struct cstl_vector *vec, size_t index)
+void *cstl_vector_at_or_null(struct cstl_vector *vec, size_t index)
 {
-  if (vec == NULL || vec->ecount <= index)
+    if (vec == NULL || vec->ecount <= index)
     {
-      return NULL;
+        return NULL;
     }
-  return vec->data + index * vec->esize;
+    return vec->data + index * vec->esize;
 }
